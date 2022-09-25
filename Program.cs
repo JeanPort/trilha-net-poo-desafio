b5e99b7b146c98e99e8aa4d1896a290d5d71b71e
Jean Port <pierreanatc522@gmail.com>
2022-09-25 12:12:35 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("519999999", "Modelo 1", "111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iPhone = new Iphone("001002022", "Modelo2", "2222222", 128);
+iPhone.Ligar();
+iPhone.InstalarAplicativo("Telegram");
+
+
+
